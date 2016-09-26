c0af422c3e391cee849654cc8a360c1688fa65fb
Chris <csjohnson79@icloud.com>
2016-09-26 19:40:57 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+
+var anyWord = prompt("Enter any word in the box below!!");
+document.write("The word you entered was " + anyWord + "---");
+
+document.write(anyWord.length + "---");
+document.write(anyWord.toLowerCase() + "---");
+document.write(anyWord.toUpperCase() + "---");
+document.write("There once was a big, fat " + anyWord + "!!---");
+document.write("Subword: " + anyWord.substring(1,4) + "---");
+// document.write(anyWord.length + "---");
+
+
+
+
+
+// var personsName = prompt("What is your name?");
+// document.write("Hello " + personsName + "!");
